5d7862aa1d0aaf3add931875b583d2e475f9e780
attharmirza <atthar.mirza@gmail.com>
2017-12-11 00:42:26 -0500

    Added Chart Title and Interactivity
    The map now reacts to hovering over states, and the title changes
    depending on the selected district maps.

--- scripts/map.js
+++ scripts/map.js
@@ -1,6 +1,7 @@
 // D3 Visualization Setup
 var width = document.getElementById('map').offsetWidth;
 var height = document.getElementById('map').offsetHeight;
+var centered;
 
 var svg = d3.select('#map').append('svg')
   .attr('width', width)
@@ -12,11 +13,13 @@ var projection = d3.geoAlbersUsa()
 
 var path = d3.geoPath().projection(projection);
 
+var g = svg.append('g');
+
 // D3 Visualization Execution
 d3.json('/data/states.json', function(error, map) {
   if (error) throw error;
 
-  var country = svg.append('g').classed('continent', true);
+  var country = g.append('g').classed('continent', true);
 
   country.selectAll()
     .data(map.features)
@@ -25,11 +28,34 @@ d3.json('/data/states.json', function(error, map) {
     .attr('d', path)
     .style('stroke', '#fff')
     .style('stroke-width', '1')
-    .style('fill', '#f2f2f2')
-    .attr('id', function (d) {
+    // .style('fill', '#f2f2f2')
+    .classed('state', true)
+    .attr('id', function(d) {
       return d.properties.NAME;
-    });
+    })
+    .on("click", clicked);
 });
+
+function clicked(d) {
+  d3.selectAll('.counties').remove();
+  d3.selectAll('.districts').remove();
+  d3.select('#titleText').remove();
+
+  state = d.properties.NAME;
+  congress = 114;
+
+  if (state !== undefined) {
+    d3.select('#title')
+      .append('p')
+      .attr('id', 'titleText')
+      .text(congress + 'th Congress, District Map of ' + state);
+  };
+
+  loadDistricts({
+    state: state,
+    congress: congress
+  });
+}
 
 // Load Districts into Visualization
 var loadDistricts = function(config) {
@@ -38,37 +64,10 @@ var loadDistricts = function(config) {
   var state = config.state;
   var congress = config.congress;
 
-  console.log(congress + 'th Congress, District Map of ' + state);
-
-  d3.json('/data/districts/' + congress + '.json', function(error, data) {
-    if (error) throw error;
-
-    var districts = svg.append('g').classed('districts', true);
-
-    var length = data.features.length;
-    var stateDistricts = [];
-
-    for (var i = 0; i < length; i++) {
-      if (data.features[i].properties.STATENAME == state) {
-        stateDistricts.push(data.features[i]);
-      }
-    }
-
-    districts.selectAll('path')
-      .data(stateDistricts)
-      // .data(data.features)
-      .enter()
-      .append('path')
-      .attr('d', path)
-      .style('stroke', '#36c87e')
-      .style('stroke-width', '1')
-      .style('fill', 'rgba(0, 0, 0, 0)');
-  });
-
   d3.json('/data/counties.json', function(error, data) {
     if (error) throw error;
 
-    var counties = svg.append('g').classed('counties', true);
+    var counties = g.append('g').classed('counties', true);
 
     var length = data.features.length;
     var stateCounties = [];
@@ -84,9 +83,37 @@ var loadDistricts = function(config) {
       .enter()
       .append('path')
       .attr('d', path)
-      .style('stroke', '#c83650')
+      .style('stroke', '#fff')
       .style('stroke-width', '1')
-      .style('fill', 'rgba(0, 0, 0, 0)');
+      .style('fill', '#e3e3e3')
+      .classed('county', true)
+      .on("click", clicked);
+  });
+
+  d3.json('/data/districts/' + congress + '.json', function(error, data) {
+    if (error) throw error;
+
+    var districts = g.append('g').classed('districts', true);
+
+    var length = data.features.length;
+    var stateDistricts = [];
+
+    for (var i = 0; i < length; i++) {
+      if (data.features[i].properties.STATENAME == state) {
+        stateDistricts.push(data.features[i]);
+      }
+    }
+
+    districts.selectAll('path')
+      .data(stateDistricts)
+      .enter()
+      .append('path')
+      .attr('d', path)
+      .style('stroke', '#36c87e')
+      .style('stroke-width', '1')
+      .style('fill', 'rgba(0, 0, 0, 0)')
+      .classed('district', true)
+      .on("click", clicked);
   });
 
   //Download SVG
